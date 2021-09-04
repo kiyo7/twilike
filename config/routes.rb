dd6c46722f4028e997cfe8c110c4af7503656961
kiyo7 <kiyo324783@icloud.com>
2021-09-04 16:01:45 +0900

    名前付きルートの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
 
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  # getルールを使って変更すると、GETリクエストが /help に送信されたときにStaticPagesコントローラーのhelpアクションを呼び出すようになる(また、help_pathやhelp_urlといった名前付きルートも使えるようになる)
+
+  #static_controller
+  get '/help', to:'static_pages#help'
+  get '/about', to:'static_pages#about'
+  get '/contact', to:'static_pages#contact'
+
+  #users_controller
+  get  '/signup', to:'users#new'
+
+  # to: '~'の後に ,as: "別名"とすると名前付きルートを変更できる
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
