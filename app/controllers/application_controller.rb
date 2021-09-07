7bb9674a0e721b56459f4b9e7f24e490216ff563
kiyo7 <kiyo324783@icloud.com>
2021-09-07 16:17:07 +0900

    [ADD]Sessionsヘルパー log_inメソッド作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  # Railsの全コントローラの親クラスであるApplicationコントローラにmoduleを読み込ませると全てのコントローラーで使えるようになる
+  include SessionsHelper
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user #sessions_helperのメソッド
+      redirect_to user #userのプロフィールページへ
     else
        flash.now[:danger] = '存在しないユーザーまたはパスワードが違います' 
     render 'new' #renderとflashを使うときは注意が必要
