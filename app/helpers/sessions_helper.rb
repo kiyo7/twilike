93f1a636320aa6756489e6fe463f95f5cd1cb908
kiyo7 <kiyo324783@icloud.com>
2021-09-08 21:57:31 +0900

    [ADD] ログアウト実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,13 +8,19 @@ module SessionsHelper
   # 現在ログイン中のユーザーを返す（いる場合）
   def current_user
     if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id]) #いたらそのまま、いなかったらDBから探す
     end
   end
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
+  end
+
+  # 現在のユーザーをログアウトする
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 
 end
