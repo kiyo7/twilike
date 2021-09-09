bd82d660b93a2775251755ea418f6271487d177a
kiyo7 <kiyo324783@icloud.com>
2021-09-09 22:43:51 +0900

    [ADD] ユーザー一覧　ページネーション機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user?, only: [:edit, :update]
+  before_action :logged_in_user?, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+
+  #User.paginate(page: 1)で30件のユーザーがヒットします.
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def new
     @user = User.new
